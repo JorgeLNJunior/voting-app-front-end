349732758ac8152c5c14acbe20ba32bcd11b6be3
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-07-31 20:59:51 -0300

    criar a app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <v-app>
-
+    <AppBar></AppBar>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
+
+import AppBar from './components/AppBar'
+
 export default {
   name: 'App',
-
+  components: {
+    AppBar
+  },
   data: () => ({
     //
   })
---
+++ src/components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar app color="transparent">
+    <h1>VoteApp</h1>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar'
+}
+</script>
+
+<style>
+
+</style>
