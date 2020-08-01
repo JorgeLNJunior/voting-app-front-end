f8e61c812436f2bbb65b32244b1db21dd825c824
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-01 14:43:10 -0300

    alterar o v-content para v-main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-content>
+  <v-main>
     <v-container fluid class="fill-height">
       <v-row align="center" justify="center">
         <v-col cols="6">
@@ -7,7 +7,7 @@
         </v-col>
       </v-row>
     </v-container>
-  </v-content>
+  </v-main>
 </template>
 
 <script>
