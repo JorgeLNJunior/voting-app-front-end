43799e36ebcdd0d8e7aa850c3b794eee900b9a4e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-07-31 21:06:27 -0300

    adicionar o vote-card a home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="home">
-
-  </div>
+  <v-content>
+    <v-container fluid class="fill-height">
+      <v-row align="center" justify="center">
+        <v-col cols="6">
+          <VoteCard></VoteCard>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
+
+import VoteCard from '../components/VoteCard'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    VoteCard
+  }
 }
 </script>
