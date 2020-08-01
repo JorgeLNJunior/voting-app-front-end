e413e4e049f684ead0475fb24ab873e3c31c0fa6
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-07-31 21:05:59 -0300

    criar estrututa inicial do vote-card

---
+++ src/components/VoteCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card shaped>
+    <v-card-title class="justify-center primary">Front-end framework</v-card-title>
+      <v-card-text class="text-center mt-6">
+        Enquete para saber a preferência de framework front-end
+        por parte dos desenvolvedores
+      </v-card-text>
+    <v-card-actions></v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VoteCard',
+  data: () => ({
+    options: ['Angular', 'React', 'Vue', 'Outro']
+  })
+}
+</script>
