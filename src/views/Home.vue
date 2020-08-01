3133ac828b0ccd82e43bd6763ea54fb8f94e71f2
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-01 16:36:17 -0300

    ajustar as colunas da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-main>
     <v-container fluid class="fill-height">
       <v-row align="center" justify="center">
-        <v-col cols="6">
+        <v-col cols="12" md="6" sm="8" xs="10">
           <VoteCard></VoteCard>
         </v-col>
       </v-row>
