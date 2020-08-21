2d635dcadc641618dfcb337e9798c58c32d08a61
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-20 22:27:05 -0300

    criar função para voto

--- src/services/SurveyService.js
+++ src/services/SurveyService.js
@@ -4,6 +4,10 @@ class SurveyService {
   async getByID (id) {
     return await http.get(`/surveys/${id}`)
   }
+
+  async vote (surveyID, optionID) {
+    return await http.post(`/surveys/${surveyID}/vote/${optionID}`)
+  }
 }
 
 export default new SurveyService()
