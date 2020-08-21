d881888ed1e404fa85fa42fb37858e1b575e0d3a
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-20 21:35:25 -0300

    fazer chamada a api e enviar o reultado para o component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
     <v-container fluid class="fill-height">
       <v-row align="center" justify="center">
         <v-col cols="12" md="6" sm="8" xs="10">
-          <VoteCard></VoteCard>
+          <VoteCard v-if="apiCallEnded" :survey="survey"></VoteCard>
+          <v-skeleton-loader v-else type="card"></v-skeleton-loader>
         </v-col>
       </v-row>
     </v-container>
@@ -13,11 +14,29 @@
 <script>
 
 import VoteCard from '../components/VoteCard'
+import SurveyService from '../services/SurveyService'
 
 export default {
   name: 'Home',
   components: {
     VoteCard
+  },
+  data: () => ({
+    survey: {},
+    apiCallEnded: false
+  }),
+  beforeMount () {
+    SurveyService.getByID(1)
+      .then((response) => {
+        this.survey = response.data.survey
+        this.apiCallEnded = true
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  methods: {
+
   }
 }
 </script>
