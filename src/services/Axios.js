2543cbe811e771c4f180b87309b20a04d8acd18e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-20 22:38:07 -0300

    corrigir url de produção da api

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const DEV_URL = 'http://localhost:3000'
-const PROD_URL = 'https://voting-app-site.netlify.app/'
+const PROD_URL = 'https://api-voting-app.herokuapp.com'
 
 const http = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL
