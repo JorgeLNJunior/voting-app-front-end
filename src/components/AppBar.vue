fcbe01087abcf70430f5b2766bc1df8b1fbf2bd7
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-16 21:03:29 -0300

    criar o navgation drawer

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app-bar app color="transparent" flat hide-on-scroll>
+    <v-app-bar-nav-icon @click="$store.navigationDrawer = !$store.navigationDrawer"></v-app-bar-nav-icon>
     <h1 class="ml-2">VotingApp</h1>
     <h1></h1>
     <v-spacer></v-spacer>
-    <v-btn class="mr-4 hidden-sm-and-down" outlined>Entrar</v-btn>
-    <RegisterDialog />
+    <v-btn class="mr-4 hidden-sm-and-down" outlined @click="logStore()">Entrar</v-btn>
+    <RegisterDialog class="hidden-sm-and-down" />
   </v-app-bar>
 </template>
 
@@ -18,7 +19,7 @@ export default {
     RegisterDialog
   },
   data: () => ({
-    formActive: false
+    //
   })
 }
 </script>
