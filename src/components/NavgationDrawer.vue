3b58de2b92aa53d6e4c4b1349a01ab1d224d021f
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-18 13:51:21 -0300

    adicionar o botão criar ao navgation drawer

--- src/components/NavgationDrawer.vue
+++ src/components/NavgationDrawer.vue
@@ -19,6 +19,11 @@
         </v-list-item>
         <v-list-item v-if="isLoged">
           <v-list-item-content>
+            <SurveyCreateDialog />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item v-if="isLoged">
+          <v-list-item-content>
             <LogoutBtn />
           </v-list-item-content>
         </v-list-item>
@@ -31,13 +36,15 @@
 import RegisterDialog from './Auth/RegisterDialog'
 import LoginDialog from './Auth/LoginDialog'
 import LogoutBtn from './Auth/LogoutBtn'
+import SurveyCreateDialog from './Survey/SurveyCreateDialog'
 
 export default {
   name: 'NavigationDrawer',
   components: {
     RegisterDialog,
     LoginDialog,
-    LogoutBtn
+    LogoutBtn,
+    SurveyCreateDialog
   },
   data: () => ({
     isLoged: false
