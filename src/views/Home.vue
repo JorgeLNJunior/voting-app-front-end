843bbd86352f331edfb6d97a3ad324f4665f3687
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-21 13:32:13 -0300

    criar home para usuário não logado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,38 @@
 <template>
   <v-main>
     <v-container fluid class="fill-height">
-      <v-col cols="6">
-        <v-row align="center" justify="center">
-          <v-col cols="12">
-            <UserSurveysCard />
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col cols="6">
-        <v-row justify="center" align="center">
-          <v-col cols="12">
-            <h1>Direita cima</h1>
-          </v-col>
-        </v-row>
-        <v-row justify="center" align="center">
-          <v-col cols="12">
-            <RamdomSurveysCard />
-          </v-col>
-        </v-row>
-      </v-col>
+      <v-row align="center" justify="center" v-if="userIsLoggedIn()">
+        <v-col cols="6">
+          <v-row align="center" justify="center">
+            <v-col cols="12">
+              <UserSurveysCard />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6">
+          <v-row justify="center" align="center">
+            <v-col cols="12">
+              <h1>Direita cima</h1>
+            </v-col>
+          </v-row>
+          <v-row justify="center" align="center">
+            <v-col cols="12">
+              <RamdomSurveysCard />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row v-else align="center" justify="center">
+        <v-col cols="8" class="text-center">
+          <h1 class="mb-6">Entre com uma conta para ver suas informações aqui!</h1>
+          <RamdomSurveysCard />
+        </v-col>
+      </v-row>
     </v-container>
   </v-main>
 </template>
 
 <script>
-
 import RamdomSurveysCard from '../components/Home/RandomSurveysCard'
 import UserSurveysCard from '../components/Home/UserSurveysCard'
 
@@ -37,6 +44,14 @@ export default {
   },
   data: () => ({
     //
-  })
+  }),
+  methods: {
+    userIsLoggedIn () {
+      if (!localStorage.getItem('AUTH_TOKEN')) {
+        return false
+      }
+      return true
+    }
+  }
 }
 </script>
