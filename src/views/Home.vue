42053c2108ad27561e734816d904c9dd9641f7e3
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-22 15:33:58 -0300

    adicionar o card de usuário a home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <v-main>
     <v-container fluid class="fill-height">
-      <v-row align="center" justify="center" v-if="userIsLoggedIn()">
+      <v-row align="start" justify="center" v-if="userIsLoggedIn()"> <!-- authenticated user -->
         <v-col cols="6">
           <v-row align="center" justify="center">
-            <v-col cols="12">
+            <v-col>
               <UserSurveysCard />
             </v-col>
           </v-row>
@@ -12,17 +12,17 @@
         <v-col cols="6">
           <v-row justify="center" align="center">
             <v-col cols="12">
-              <h1>Direita cima</h1>
+              <RamdomSurveysCard />
             </v-col>
           </v-row>
           <v-row justify="center" align="center">
             <v-col cols="12">
-              <RamdomSurveysCard />
+              <UserCard />
             </v-col>
           </v-row>
         </v-col>
       </v-row>
-      <v-row v-else align="center" justify="center">
+      <v-row v-else align="center" justify="center"> <!-- unauthenticated user -->
         <v-col cols="8" class="text-center">
           <h1 class="mb-6">Entre com uma conta para ver suas informações aqui!</h1>
           <RamdomSurveysCard />
@@ -35,12 +35,14 @@
 <script>
 import RamdomSurveysCard from '../components/Home/RandomSurveysCard'
 import UserSurveysCard from '../components/Home/UserSurveysCard'
+import UserCard from '../components/Home/UserCard'
 
 export default {
   name: 'Home',
   components: {
     RamdomSurveysCard,
-    UserSurveysCard
+    UserSurveysCard,
+    UserCard
   },
   data: () => ({
     //
